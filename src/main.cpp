ca41bbb114af8f51b90db1060e1e21fc70abb024
90degs2infty <90degs2infty@posteo.org>
2023-03-17 12:11:39 +0100

    Fix main's return type

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <util_A/greet.h>
 #include <iostream>
 
-void main() {
+int main() {
     std::cout << "Welcome to application main from project parent_1.\n\n";
 
     greet_world();
+
+    return 0;
 }
